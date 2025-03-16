665bd381c523b321a2f58202e0a693cddb60dfd8
KOHEI <ko.08financier.2mitchan4@gmail.com>
2025-03-16 21:24:36 +0900

    fix: CSSの調整

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
+
   def configure_permitted_parameters
     # 新規登録時にユーザーネームを登録できるようストロングパラメーターを追加
     devise_parameter_sanitizer.permit(:sign_up, keys: [ :name ])
