9bce1dd90a1e8b7558e8265335e8d0760ad35adb
KOHEI <ko.08financier.2mitchan4@gmail.com>
2025-06-02 22:18:19 +0900

    delete: お気に入りフォルダの作成を本リリース後に移動

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,8 +2,5 @@ class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :technique
 
-  has_many :folder_favorites, dependent: :destroy
-  has_many :folders, through: :folder_favorites
-
   validates :user_id, uniqueness: { scope: :technique_id }
 end
--- app/models/folder_favorite.rb
+++
@@ -1,6 +0,0 @@
-class FolderFavorite < ApplicationRecord
-  belongs_to :folder
-  belongs_to :favorite
-
-  validates :folder_id, uniqueness: { scope: :favorite_id }
-end
