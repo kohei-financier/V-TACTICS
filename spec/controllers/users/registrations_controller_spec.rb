9b198fd06a2fedbc0f9e65cbab5d7c1e96fe38af
KOHEI <ko.08financier.2mitchan4@gmail.com>
2025-06-16 00:33:08 +0900

    add: 新規登録のRspec追加

---
+++ spec/controllers/users/registrations_controller_spec.rb
@@ -0,0 +1,28 @@
+require "rails_helper"
+
+RSpec.describe "Devise新規登録", type: :controller do
+
+  before do
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+  end
+
+  describe "configure_sign_up_params" do
+    it "configure_sign_up_params" do
+      controller = Users::RegistrationsController.new
+      allow(controller).to receive(:devise_parameter_sanitizer)
+
+      expect(controller.devise_parameter_sanitizer).to receive(:permit).with(:sign_up, keys: [ :attribute ])
+      controller.send(:configure_sign_up_params)
+    end
+  end
+
+  describe "configure_account_update_params" do
+    it "configure_account_update_params" do
+      controller = Users::RegistrationsController.new
+      allow(controller).to receive(:devise_parameter_sanitizer)
+
+      expect(controller.devise_parameter_sanitizer).to receive(:permit).with(:account_update, keys: [ :attribute ])
+      controller.send(:configure_account_update_params)
+    end
+  end
+end
