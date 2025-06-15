31a29d7fa99b5bde104edf96a21cd21583633653
KOHEI <ko.08financier.2mitchan4@gmail.com>
2025-06-15 16:34:41 +0900

    add: ログインできない場合のテスト実装

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -13,5 +13,15 @@ RSpec.describe "Users", type: :system do
         expect(page).to have_content "ログインしました"
       end
     end
+
+    context "不正入力の場合" do
+      it "ログイン不可能" do
+        visit new_user_session_path
+        fill_in "メールアドレス", with: user.email
+        fill_in "パスワード", with: "wrong_password"
+        click_button "ログイン"
+        expect(page).to have_content "メールアドレス もしくはパスワードが不正です。"
+      end
+    end
   end
 end
