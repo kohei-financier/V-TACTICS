9fccf6aedcf1d5c9492c26b748ea345dfabbc8e9
KOHEI <ko.08financier.2mitchan4@gmail.com>
2025-07-25 00:00:05 +0900

    add: お気に入りajax

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,12 +2,12 @@ class FavoritesController < ApplicationController
   def create
     technique = Technique.find(params[:technique_id])
     current_user.favorite(technique)
-    redirect_to techniques_path, success: "お気に入りに追加しました：マイページで確認できます"
+    # redirect_to techniques_path, success: "お気に入りに追加しました：マイページで確認できます"
   end
 
   def destroy
     technique = current_user.favorites.find(params[:id]).technique
     current_user.unfavorite(technique)
-    redirect_to techniques_path, success: "お気に入りを解除しました", status: :see_other
+    # redirect_to techniques_path, success: "お気に入りを解除しました", status: :see_other
   end
 end
