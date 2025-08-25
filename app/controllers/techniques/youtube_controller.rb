85438918189cfc45fead683db71b051c9197e796
KOHEI <ko.08financier.2mitchan4@gmail.com>
2025-08-25 15:35:07 +0900

    fix: youtubeのID抜き出しの正規表現を投稿フォームのプレビューの正規表現と統一

--- app/controllers/techniques/youtube_controller.rb
+++ app/controllers/techniques/youtube_controller.rb
@@ -22,7 +22,7 @@ class Techniques::YoutubeController < ApplicationController
 
     # youtubeAPIを引っ張ってくる
     youtube_service = YoutubeService.new
-    @movie_title = youtube_service.get_movie_title(@technique.only_id_from_youtube_url)
+    @movie_title = youtube_service.get_movie_title(@technique.youtube_video_id)
 
     # おすすめ動画のためにIDをランダムで引っ張ってくる
     recommend_id = Technique.where(source_type: "youtube").where.not(id: @technique.id).pluck(:id).sample(5)
